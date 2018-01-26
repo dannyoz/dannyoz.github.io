3b38faed36543399f6a14aae811131be5ff504b3
Dan Osborne <dan.osborne@rocketmail.com>
2018-01-26 14:14:40 +0000

    Basic component changes

---
+++ app/components/intro/intro.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="intro">
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: 'Intro',
+            }
+        }
+    }
+</script>
---
+++ app/components/main.vue
@@ -0,0 +1,12 @@
+<template>
+	<intro></intro>
+</template>
+
+<script>
+    import intro from './intro/intro.vue';
+	export default {
+		components: {
+            intro,
+        },
+	};
+</script>
