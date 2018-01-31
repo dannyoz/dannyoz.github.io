cb91876b9c9a5d8deabff68253babbc59185e891
Dan Osborne <dan.osborne@rocketmail.com>
2018-01-31 10:21:33 +0000

    Added project files and gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
-var gulp        = require('gulp');
-var gbrowserify = require('gulp-browserify');
-var browserify  = require('browserify');
-var babelify    = require("babelify");
-var vueify      = require('vueify')
-var stripDebug  = require('gulp-strip-debug');
-var uglify      = require('gulp-uglify');
-var sass        = require('gulp-sass');
-var htmlmin     = require('gulp-htmlmin');
+const gulp = require('gulp');
+const gbrowserify = require('gulp-browserify');
+const browserify = require('browserify');
+const babelify = require("babelify");
+const vueify = require('vueify')
+const stripDebug = require('gulp-strip-debug');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const htmlmin = require('gulp-htmlmin');
+const fs = require('fs');
 
-gulp.task('img',function(){
+gulp.task('img', () => {
     gulp.src('./app/img/**/*.*',{base : './app'})
         .pipe(gulp.dest('./environments/development/'))
         .pipe(gulp.dest('./environments/production/'));
 });
 
-gulp.task('browserify', function () {
+gulp.task('browserify', () =>{
     gulp.src('./app/app.js', {entry: true})
         .pipe(gbrowserify({
             transform: ['vueify','babelify']
@@ -24,20 +25,27 @@ gulp.task('browserify', function () {
         .pipe(gulp.dest('./build/js'))
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
     gulp.src('./app/styles.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('index.html', ['frontEnd']);
-    gulp.watch('app/**/*.scss', ['frontEnd']);
-    gulp.watch('app/**/*.js', ['frontEnd']);
-    gulp.watch('app/**/*.vue', ['frontEnd']);
-    gulp.watch('app/**/*.jsx', ['frontEnd']);
+gulp.task('project-file', () => {
+    const projects = fs.readdirSync('./app/projects').filter((value) => {
+        return value !== '.DS_Store';
+    });
+    console.log(projects);
 });
+
+gulp.task('watch', () => {
+    gulp.watch('index.html', ['frontEnd']);
+    gulp.watch('app/**/*.scss', ['sass']);
+    gulp.watch('app/**/*.js', ['browserify']);
+    gulp.watch('app/**/*.vue', ['frontEnd']);
+});
+
 
 gulp.task('frontEnd', ['browserify', 'sass', 'img']);
 gulp.task('default',  ['browserify', 'sass', 'img', 'watch']);
