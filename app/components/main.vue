e2f42f1626b95a27c99fc835d17c0316b2aef700
Dan Osborne <dan.osborne@rocketmail.com>
2018-03-01 17:19:06 +0000

    Started work on view switcher

--- app/components/main.vue
+++ app/components/main.vue
@@ -1,15 +1,18 @@
 <template>
 	<div>
+		<switcher></switcher>
 		<intro></intro>
 		<projects></projects>
 	</div>
 </template>
 
 <script>
+	import switcher from './switcher/switcher.vue';
     import intro from './intro/intro.vue';
     import projects from './projects/projects.vue';
 	export default {
 		components: {
+			switcher,
             intro,
             projects,
         },
---
+++ app/components/switcher/switcher.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="switcher">
+		<p>{{currentView}}</p>
+		<button @click="swtichView('1')">1</button>
+		<button @click="swtichView('2')">2</button>
+		<button @click="swtichView('3')">3</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				currentView: null,
+			}
+		},
+		mounted() {
+			console.log(location);
+		},
+		methods: {
+			swtichView(view) {
+				this.currentView = view;
+			}
+		}
+	}
+</script>
