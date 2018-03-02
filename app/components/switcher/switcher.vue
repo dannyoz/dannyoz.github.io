9f5a8eaa506028309bd9ad652d883d9cbc5dd20d
Dan Osborne <dan.osborne@rocketmail.com>
2018-03-02 16:29:14 +0000

    Switcher animation

--- app/components/switcher/switcher.vue
+++ app/components/switcher/switcher.vue
@@ -1,25 +1,71 @@
 <template>
-	<div class="switcher">
-		<p>{{currentView}}</p>
-		<button @click="swtichView('1')">1</button>
-		<button @click="swtichView('2')">2</button>
-		<button @click="swtichView('3')">3</button>
+	<div class="switcher" :class="animState">
+		<div class="switcher__view current-view">
+			<p class="centred">{{currentView}}</p>
+		</div>
+
+		<div class="switcher__view next-view">
+			<p class="centred">{{nextView}}</p>
+		</div>
+		
+		<nav>
+			<button @click="swtichView('1')">1</button>
+			<button @click="swtichView('2')">2</button>
+			<button @click="swtichView('3')">3</button>
+			<button @click="swtichView('4')">4</button>
+		</nav>
 	</div>
 </template>
 
 <script>
+	const transitionDuration = 1000; 
 	export default {
 		data() {
 			return {
 				currentView: null,
+				nextView: null,
+				animState: null,
 			}
 		},
 		mounted() {
-			console.log(location);
+			if(location.hash.length) {
+				this.handleHash();
+			}
+
+			// TODO: fix this broken shit
+			// window.addEventListener('popstate', () => {
+			// 	console.log('Hash changed!!!');
+			// 	this.handleHash();
+			// });
 		},
 		methods: {
 			swtichView(view) {
+				// this.currentView = view;
+				// const hash = `#/${view}`;
+				// history.pushState({}, view, hash);
+				this.nextView = view;
+				this.animState = 'exit';
+
+				setTimeout(() => {
+					this.animState = 'enter';
+					this.updateCurrentView(view);
+
+					setTimeout(() => {
+						this.nextView = null;
+						this.animState = null;
+					}, transitionDuration);
+				}, transitionDuration);
+			},
+
+			updateCurrentView(view) {
 				this.currentView = view;
+				const hash = `#/${view}`;
+				history.pushState({}, view, hash);
+			},
+
+			handleHash() {
+				const view = location.hash.replace('#/', '');
+				this.updateCurrentView(view);
 			}
 		}
 	}
