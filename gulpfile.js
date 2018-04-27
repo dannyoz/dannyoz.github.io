1c4e2d4ebb6a16abafef56cb42dfb02d3c444bb1
Dan Osborne <dan.osborne@rocketmail.com>
2018-04-27 10:16:00 +0100

    remane gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,7 @@ gulp.task('project-file', () => {
 
 });
 
-gulp.task('serve', () => {
+gulp.task('server', () => {
     connect().use(serveStatic(__dirname)).listen(8080, function(){
         console.log('Server running on 8080...');
     });
